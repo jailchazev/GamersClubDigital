49db356420b1d225e05684e099696ad252c42315
jailchazev <145948981+jailchazev@users.noreply.github.com>
2025-09-23 08:07:08 -0500

    Agregado sitemap.xml y ruta en Flask"

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 # app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory('.', 'sitemap.xml')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
